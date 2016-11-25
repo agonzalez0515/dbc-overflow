35db39f738d5cf0dae1853677f819ddac409bcf9
Julia Dolgoszyja <juliadolgoszyja@gmail.com>
2016-11-25 15:08:02 -0800

    Create questions controller routes.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,12 +4,35 @@ get 'questions/:id' do
   @votes = @question.votes
   # we can use this method inside our question/show view or here
   @value_total = votes_value(@votes)
+  erb :'/questions/show'
+end
+
+get 'questions/new' do
+  erb :'/questions/new'
 end
 
 post '/questions' do
   @question = Question.new(params[:question], user_id: current_user.id)
   if @post.save
-    erb :'/questions/show'
+    redirect "/questions/#{@question.id}"
   else
-    @error =
+    @error = "Something went wrong! Please, try again"
+    redirect '/'
+  end
+end
+
+get 'questions/:id/edit' do
+  erb :'/questions/edit'
+end
+
+put 'questions/:id' do
+  @question = Question.find(params[:id])
+  @question.update(params[:question])
+  erb :'/questions/show'
+end
+
+delete 'questions/:id' do
+  @question = Question.find(params[:id])
+  @question.destroy
+  redirect '/'
 end
