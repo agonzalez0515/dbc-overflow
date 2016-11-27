257aa01b87756081a0363c449c8cc4753e39e3b7
agonzalez0515 <agonzalez0515@gmail.com>
2016-11-27 15:39:28 -0800

    appends new question with ajax to index page

--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,25 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+$('.ask-question-form').on('submit', '.new-question', function(event){
+  event.preventDefault();
+
+    var url = $(this).attr('action')
+
+    var data =$('.new-question').serialize()
+
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: data
+    })
+
+    .done(function(response){
+      $('.question-container').prepend(response);
+    })
+})
+
+
+
 });
